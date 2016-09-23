fb098b3f8faa4a4ab05d607c11719651b2106fc7
PadmapriyaKumarasamy <padmapriya.kumarasamy@lnttechservices.com>
2016-09-23 12:28:39 +0530

    Upated Header file
    Signed-off-by: PadmapriyaKumarasamy <padmapriya.kumarasamy@lnttechservices.com>

--- gst-westerosvideosink/src/gstwesterosvideosink.h
+++ gst-westerosvideosink/src/gstwesterosvideosink.h
@@ -7,7 +7,27 @@
 #ifndef __GST_WESTEROSVIDEOSINK_H__
 #define __GST_WESTEROSVIDEOSINK_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+
 #include <gst/gst.h>
+#include <gst/video/video.h>
+#include <gst/video/gstvideosink.h>
+#include <gst/video/gstvideometa.h>
+
+#include <wayland-client.h>
+
+
 
 G_BEGIN_DECLS
 
@@ -26,13 +46,59 @@ G_BEGIN_DECLS
 typedef struct _GstwesterosVideoSink      GstwesterosVideoSink;
 typedef struct _GstwesterosVideoSinkClass GstwesterosVideoSinkClass;
 
+enum
+{
+  PROP_0,
+  PROP_WAYLAND_DISPLAY
+};
+
+
+struct westeros_window_display
+{
+  struct wl_display *wos_display;
+  struct wl_registry *wos_registry;
+  struct wl_compositor *wos_compositor;
+  struct wl_shell *wos_shell;
+  struct wl_shm *wos_shm;
+  uint32_t wos_formats;
+
+  int width, height;
+  struct wl_surface *wos_surface;
+  struct wl_shell_surface *wos_shell_surface;
+  struct wl_buffer *wos_buffer;
+  struct wl_callback *wos_callback;
+  //guint redraw_pending :1;
+  guint redraw_check :1;
+
+};
+
+
+struct wos_shared_pool {
+  struct wl_shm_pool *pool;
+  size_t size;
+  size_t count;
+  void *buff;
+};
+
+typedef westeros_window_display wDisplay;
+
+
+
 struct _GstwesterosVideoSink
 {
-  GstElement element;
+  GstVideoSink parent;
 
-  GstPad *sinkpad, *srcpad;
+  wDisplay *window;
+  struct wos_shared_pool *shared_pool;
 
-  gboolean silent;
+  GstBufferPool *pool;
+
+  GMutex wos_lock;
+
+  gint VWidth;
+  gint VHeight;
+  uint32_t format;
+
 };
 
 struct _GstwesterosVideoSinkClass 
